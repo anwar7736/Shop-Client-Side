1c3ee16957ad87effa378ae19a9dfc5664ed592c
anwar7736 <anwarhossain7736@gmail.com>
2021-07-14 15:09:35 +0600

    A

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,13 @@
 import React, {Component,Fragment} from 'react';
 
-class Footer extends Component {
+class Footer extends React.Component {
     render() {
         return (
             <Fragment>
                     <div className="container-fluid mt-5 ">
                         <div className="row mt-5">
                             <div className="col-12 p-3  mt-5 text-center shadow-sm bg-site ">
-                                    <p className="product-name text-white no-print">All Rights Reserved By Md Anwar Hossain</p>
+                                    <p className="product-name text-white no-print">All Rights Reserved By All Group 02 Members</p>
                             </div>
                         </div>
                     </div>
